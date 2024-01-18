c2b4fdbf82c21f00b9e95990f71cf29b37e6b8d5
Roman <roomen817@gmail.com>
2024-01-18 23:18:10 +0100

    + cors

--- src/main/java/com/roman/forum/config/CorsConfig.java
+++ src/main/java/com/roman/forum/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         configuration.setAllowCredentials(true);
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://vatra.netlify.app/"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
